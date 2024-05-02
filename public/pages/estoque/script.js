8889197ca56a4e3d19a22f18ae795258168c916e
Diomar Gonçalves <50972479+DiomarBR@users.noreply.github.com>
2024-05-02 17:25:54 -0300

    update index stock

--- public/pages/estoque/script.js
+++ public/pages/estoque/script.js
@@ -1,11 +1,47 @@
-const ThemeSwitcher = {
-    init: function () {
-      // Evento para alternar o tema
-      const themeSwitch = document.getElementById("theme-switch");
-      const body = document.body;
-      themeSwitch.addEventListener("change", () => {
-        body.classList.toggle("dark-mode");
+const stockApp = {
+  init: function () {
+
+    this.loadProductList();
+
+    document
+      .getElementById("stock-movement-btn")
+      .addEventListener("click", this.openModal);
+    document.querySelector(".close").addEventListener("click", this.closeModal);
+  },
+
+  openModal: function () {
+    const modal = document.getElementById("stock-movement-modal");
+    modal.style.display = "block";
+  },
+
+  closeModal: function () {
+    const modal = document.getElementById("stock-movement-modal");
+    modal.style.display = "none";
+  },
+  loadProductList: async function () {
+    try {
+      const response = await fetch("http://localhost:3000/products");
+      if (!response.ok) {
+        throw new Error("Erro ao carregar a lista de produtos.");
+      }
+      const data = await response.json();
+      const productList = document.getElementById("product-stock");
+      productList.innerHTML = "";
+      data.forEach((product) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+          <td>${product.id}</td>
+          <td>${product.name}</td>
+          <td>${product.cost_price}</td>
+          <td>${product.sale_price}</td>
+          <td>${product.barcode}</td>
+          <td><button class="edit-button" data-product-id="${product.id}">Editar</button></td>
+        `;
+        productList.appendChild(row);
       });
-    },
-  };
-  ThemeSwitcher.init();
+    } catch (error) {
+      console.error("Erro:", error.message);
+    }
+  },
+};
+stockApp.init();
--- public/pages/produtos/script.js
+++ public/pages/produtos/script.js
@@ -1,14 +1,3 @@
-const ThemeSwitcher = {
-  init: function () {
-    // Evento para alternar o tema
-    const themeSwitch = document.getElementById("theme-switch");
-    const body = document.body;
-    themeSwitch.addEventListener("change", () => {
-      body.classList.toggle("dark-mode");
-    });
-  },
-};
-ThemeSwitcher.init();
 
 const productApp = {
   init: function () {
